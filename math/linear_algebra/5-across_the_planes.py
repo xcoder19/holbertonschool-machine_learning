dc6ab745601b78a3f36c5eceab6bb1bb5802ae90
hedi <hedihamida98@gmail.com>
2023-03-23 18:12:57 +0100

    add 2d matrices

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""add_matrice2d"""
+
+
+def add_matrices2D(mat1, mat2):
+    """add_matrice2d"""
+    matrix = []
+    rows = len(mat1)
+    cols = len(mat1[0])
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    for i in range(rows):
+        arr = []
+    for j in range(cols):
+        arr.append(mat1[i][j] + mat2[i][j])
+    matrix.append(arr)
+
+    return matrix
+
+
+def matrix_shape(matrix):
+    """ matrix shape"""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
