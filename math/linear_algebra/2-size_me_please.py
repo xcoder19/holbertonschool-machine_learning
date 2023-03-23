7093e01d25b997a64794a9c619a869667e8661ef
hedi <hedihamida98@gmail.com>
2023-03-23 15:37:08 +0100

    shape of matrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,16 @@
+def matrix_shape(matrix):
+    rows = len(matrix)
+    cols = 0
+    cols2 = 0
+    for i in range(0,rows):
+        if cols < len(matrix[i]):
+            cols = len(matrix[i])
+        for j in matrix[i]:
+            if isinstance(j,list) and cols2 < len(j):
+                cols2 = len(j)
+    if cols2 > 0:
+        shape = [rows,cols,cols2]
+    else:
+        shape = [rows,cols]
+    return shape
+    
