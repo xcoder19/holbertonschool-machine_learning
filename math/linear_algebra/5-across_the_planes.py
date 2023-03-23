121dbb31e01e12175a8adb733d753c3751fc3ac1
hedi <hedihamida98@gmail.com>
2023-03-23 18:23:25 +0100

    adding two 2d matrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -5,15 +5,20 @@
 def add_matrices2D(mat1, mat2):
     """add_matrice2d"""
     matrix = []
+    if (len(mat1) == 0 and len(mat2) == 0):
+        return matrix
+
     rows = len(mat1)
     cols = len(mat1[0])
+
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
     for i in range(rows):
         arr = []
-    for j in range(cols):
-        arr.append(mat1[i][j] + mat2[i][j])
-    matrix.append(arr)
+        for j in range(cols):
+            arr.append(mat1[i][j] + mat2[i][j])
+
+        matrix.append(arr)
 
     return matrix
 
@@ -21,8 +26,7 @@ def add_matrices2D(mat1, mat2):
 def matrix_shape(matrix):
     """ matrix shape"""
     shape = []
-    if len(matrix) == 0:
-        return None
+
     while isinstance(matrix, list):
         shape.append(len(matrix))
         matrix = matrix[0]
