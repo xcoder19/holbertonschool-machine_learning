29f47b446973525e24dfd0ecdd2a27ca21744eb0
hedi <hedihamida98@gmail.com>
2023-03-23 15:58:21 +0100

    new implementation of matrix shape

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,23 +3,7 @@
 
 
 def matrix_shape(matrix):
-    # """ matrix shape"""
-    # rows = len(matrix)
-    # cols = 0
-    # cols2 = 0
-    # for i in range(0, rows):
-    #     if isinstance(matrix[i], list) and cols < len(matrix[i]):
-    #         cols = len(matrix[i])
-    #         for j in matrix[i]:
-    #             if isinstance(j, list) and cols2 < len(j):
-    #                 cols2 = len(j)
-    # if cols2 > 0:
-    #     shape = [rows, cols, cols2]
-    # elif cols > 0:
-    #     shape = [rows, cols]
-    # else:
-    #     shape = [rows]
-    # return shape
+        """ matrix shape"""
         shape = []
         while isinstance(matrix, list):
             shape.append(len(matrix))
