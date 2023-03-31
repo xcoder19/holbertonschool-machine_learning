a9fed69d369cf8d2c52e1d060e607f3ce38f1f09
hedi <hedihamida98@gmail.com>
2023-03-31 23:38:21 +0100

    poly derivatif

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""poly_derivative"""
+
+
+def poly_derivative(poly):
+    """poly_derivative"""
+
+    if not isinstance(
+        poly,
+        list) or any(
+        not isinstance(
+            coef,
+            int) for coef in poly):
+        return None
+
+    if len(poly) <= 1:
+        return [0]
+
+    derivative = []
+    for i in range(1, len(poly)):
+        derivative.append(i * poly[i])
+
+    return derivative
