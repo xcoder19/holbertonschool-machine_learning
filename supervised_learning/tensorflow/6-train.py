292f7f0b6160d63ee5d2610e58cbca053c1c92f8
hedi <hedihamida98@gmail.com>
2023-04-21 22:07:03 +0100

    train

---
+++ supervised_learning/tensorflow/6-train.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+"""evaluate"""
+
+import tensorflow as tf
+import numpy as np
+
+
+def train(
+        X_train,
+        Y_train,
+        X_valid,
+        Y_valid,
+        layer_sizes,
+        activations,
+        alpha,
+        iterations,
+        save_path="/tmp/model.ckpt"):
+
+    calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+    calculate_loss = __import__('4-calculate_loss').calculate_loss
+    create_placeholders = __import__(
+        '0-create_placeholders').create_placeholders
+    create_train_op = __import__('5-create_train_op').create_train_op
+    forward_prop = __import__('2-forward_prop').forward_prop
+
+    nx, m = X_train.shape
+    _, classes = Y_train.shape
+
+    x, y = create_placeholders(nx, classes)
+    y_pred = forward_prop(x, layer_sizes, activations)
+    loss = calculate_loss(y, y_pred)
+    accuracy = calculate_accuracy(y, y_pred)
+    train_op = create_train_op(loss, alpha)
+
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    tf.add_to_collection('y_pred', y_pred)
+    tf.add_to_collection('loss', loss)
+    tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('train_op', train_op)
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(init)
+
+        for i in range(iterations + 1):
+            sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+
+            if i % 100 == 0:
+                training_cost, training_accuracy = sess.run(
+                    [loss, accuracy], feed_dict={x: X_train, y: Y_train})
+                validation_cost, validation_accuracy = sess.run(
+                    [loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(training_cost))
+                print("\tTraining Accuracy: {}".format(training_accuracy))
+                print("\tValidation Cost: {}".format(validation_cost))
+                print("\tValidation Accuracy: {}".format(validation_accuracy))
+
+        save_path = saver.save(sess, save_path)
+
+    return save_path
