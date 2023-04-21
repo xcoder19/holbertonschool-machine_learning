f58b0a51b1062b4083cc700d6023d45f7814a4ab
hedi <hedihamida98@gmail.com>
2023-04-21 19:13:31 +0100

    layer method

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -8,8 +8,8 @@ def create_layer(prev, n, activation):
     initializer = tf.contrib.layers.variance_scaling_initializer(
         mode="FAN_AVG")
 
-    layer = tf.layers.dense(
-
+    layer = tf.layers.Dense(
+        inputs=prev,
         units=n,
         activation=activation,
         kernel_initializer=initializer,
