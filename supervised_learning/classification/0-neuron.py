0ad4e335d6b11dd7b0b017cc9107159e70cf8bba
hedi <hedihamida98@gmail.com>
2023-04-21 14:30:27 +0100

    neuron class

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import numpy as np
+
 
 class Neuron:
 
@@ -8,7 +10,7 @@ class Neuron:
             raise TypeError("nx must be an integer")
         elif nx < 1:
             raise ValueError("nx must be a positive integer")
-        
-        self.W = 0
+
+        self.W = np.random.randn(nx).reshape(1, nx)
         self.b = 0
         self.A = 0
