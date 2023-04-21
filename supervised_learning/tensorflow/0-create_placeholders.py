20187729189f80525c42ac560b5581e12aa3bf41
hedi <hedihamida98@gmail.com>
2023-04-21 19:02:05 +0100

    tensorflow placeholder method

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" placeholders"""
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """placeholder method"""
+    x = tf.placeholder(dtype=tf.float32, shape=(nx, None), name='x')
+    y = tf.placeholder(dtype=tf.float32, shape=(classes, None), name='y')
+    return x, y
