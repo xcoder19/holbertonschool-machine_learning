b6c85009a685c5a5f1b6f2492854163e49f9bab4
xcoder19 <hedihamida98@gmail.com>
2023-06-13 18:52:07 +0100

    evaluate method

---
+++ supervised_learning/classification/12-neural_network.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""neural network"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """neural network"""
+
+    def __init__(self, nx, nodes):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        elif nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """getter w1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """getter b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """getter a1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """getter w2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """getter b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """getter a2"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """forward prop"""
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """cost method"""
+        m = Y.shape[1]
+        cost = (-1 / m) * np.sum(Y * np.log(A) + (1 - Y) *
+                                 np.log(1.0000001 - A)) + 0.0000000000000001
+
+        return cost
+
+    def evaluate(self, X, Y):
+        """evaluate"""
+        _, A = self.forward_prop(X)
+        prediction = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return prediction, cost
