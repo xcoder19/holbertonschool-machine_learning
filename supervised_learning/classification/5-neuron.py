a9f747b0619ff1cc03582c14f7fd2e3bd61ef3a4
xcoder19 <hedihamida98@gmail.com>
2023-06-13 11:42:43 +0100

    gradient descent

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""Neuron class"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron class for supervised ml"""
+
+    def __init__(self, nx):
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(nx).reshape(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter for A"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """forward prop"""
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """cost method"""
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(Y * np.log(A) +
+                                 (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """evaluate"""
+
+        self.forward_prop(X)
+        c = self.cost(Y, self.__A)
+        A = np.where(self.__A >= 0.5, 1, 0)
+        return A, c
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """gradient descent"""
+        m = X.shape[1]
+
+        dZ = A - Y
+        dW = np.matmul(X, dZ.T) / m
+        db = np.sum(dZ) / m
+
+        self.__W = self.__W - alpha * dW.T
+        self.__b = self.__b - alpha * db
