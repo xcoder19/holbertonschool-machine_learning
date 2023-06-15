b8ca99b68ea1303723529d1adcd6d423fb888630
xcoder19 <hedihamida98@gmail.com>
2023-06-15 19:44:40 +0100

    docs

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -4,6 +4,8 @@ import tensorflow.keras as K
 
 
 class Yolo:
+    """yolo class"""
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = K.models.load_model(model_path)
         self.class_names = self.load_classes(classes_path)
