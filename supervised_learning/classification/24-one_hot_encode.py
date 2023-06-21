b1df47830bad47bd41cf087de02045b2952d102e
xcoder19 <hedihamida98@gmail.com>
2023-06-21 13:11:39 +0100

    encode decode

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+    one_hot_encode
+    """
+    if not isinstance(Y, np.ndarray) or not isinstance(classes, int):
+        return None
+    try:
+        m = Y.shape[0]
+        one_hot = np.zeros((classes, m))
+        one_hot[Y, np.arange(m)] = 1
+        return one_hot
+    except Exception:
+        return None
---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """
+    one_hot_decode
+    """
+    if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
+        return None
+    try:
+        return np.argmax(one_hot, axis=0)
+    except Exception:
+        return None
