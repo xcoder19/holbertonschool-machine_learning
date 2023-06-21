4769de5d01078fcf8125b747f806674182bb03f7
xcoder19 <hedihamida98@gmail.com>
2023-06-21 10:48:23 +0100

    cost for deep nn

--- supervised_learning/classification/19-deep_neural_network.py
+++ supervised_learning/classification/19-deep_neural_network.py
@@ -66,5 +66,7 @@ class DeepNeuralNetwork:
     def cost(self, Y, A):
         """ calculates the cost """
         m = Y.shape[1]
-        cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        term1 = Y * np.log(A)
+        term2 = (1 - Y) * np.log(1.0000001 - A)
+        cost = -(1 / m) * np.sum(term1 + term2)
         return cost
