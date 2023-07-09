779dedfa8debde7896cc305c6511ab78ba791420
xcoder19 <hedihamida98@gmail.com>
2023-07-09 22:46:56 +0100

    pca

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""pca"""
+"""pca v2 """
 
 import numpy as np
 
 
 def pca(X, ndim):
-    """pca"""
+    """pca v2"""
     X_normalized = X - np.mean(X, axis=0)
 
     _, _, right_singular_vectors = np.linalg.svd(X_normalized)
