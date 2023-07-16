721dcd8d9958cc0f7d8365e3e6a95f34fb75eebf
xcoder19 <hedihamida98@gmail.com>
2023-07-16 20:24:51 +0100

    optimum

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -3,6 +3,10 @@
 import numpy as np
 
 
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """optimum"""
 
