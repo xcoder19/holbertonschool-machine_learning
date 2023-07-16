328789c2b8f57ec69d7bc6ddce59078536bf7f6a
xcoder19 <hedihamida98@gmail.com>
2023-07-16 20:30:34 +0100

    docs

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""PDF """
+"""calculates the probability density function of a Gaussian distribution"""
 import numpy as np
 
 
 def pdf(X, m, S):
-    """PDF """
+    """calculates the probability density function of a Gaussian distribution"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
     if not isinstance(m, np.ndarray) or len(m.shape) != 1:
