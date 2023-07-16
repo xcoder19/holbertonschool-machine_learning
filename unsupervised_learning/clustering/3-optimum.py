0ae8f690bd6a4f4d954f30d41bd24745e7457720
xcoder19 <hedihamida98@gmail.com>
2023-07-16 20:23:27 +0100

    optimum

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -2,9 +2,6 @@
 """optimum"""
 import numpy as np
 
-variance = __import__("2-variance").variance
-kmeans = __import__("1-kmeans").kmeans
-
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """optimum"""
@@ -38,3 +35,71 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
               for i in range(len(variances))]
 
     return results, d_vars
+
+
+def variance(X, C):
+    """variance"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(C, np.ndarray) or len(C.shape) != 2:
+        return None
+
+    try:
+        distances = np.linalg.norm(X[:, None] - C, axis=-1)
+
+        min_distances = np.min(distances, axis=-1)
+
+        var = np.sum(min_distances ** 2)
+    except Exception:
+        return None
+
+    return var
+
+
+def kmeans(X, k, iterations=1000):
+    """kmeans"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+
+    n, d = X.shape
+    C = initialize(X, k)
+
+    for _ in range(iterations):
+        C_old = C.copy()
+
+        distances = np.linalg.norm(X[:, None] - C, axis=-1)
+
+        clss = np.argmin(distances, axis=-1)
+
+        for j in range(k):
+            if X[clss == j].size == 0:
+                C[j] = initialize(X, 1)
+            else:
+                C[j] = np.mean(X[clss == j], axis=0)
+
+        if np.all(C_old == C):
+            break
+
+    return C, clss
+
+
+def initialize(X, k):
+    """Initialize """
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+
+    n, d = X.shape
+
+    min_vals = np.min(X, axis=0)
+    max_vals = np.max(X, axis=0)
+
+    centroids = np.random.uniform(min_vals, max_vals, (k, d))
+
+    return centroids
