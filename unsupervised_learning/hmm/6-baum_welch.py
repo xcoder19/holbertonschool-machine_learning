f9eb9e6783daeab2827241dbbe5728dd58dd0440
xcoder19 <hedihamida98@gmail.com>
2023-07-23 21:24:25 +0100

    baum

---
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env pyton3
+
+"""baum_welch"""
+import numpy as np
+
+
+def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
+    """baum_welch"""
+
+    return Transition, Emission
