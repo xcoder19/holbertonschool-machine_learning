3fda68c4d3435ff0ec193d320db679735d2d03a2
xcoder19 <hedihamida98@gmail.com>
2023-08-06 21:12:54 +0100

    sparse autoencoder

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -11,10 +11,14 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     for nodes in hidden_layers:
         encoder_layers.append(keras.layers.Dense(
             nodes, activation='relu')(encoder_input))
-    encoder_output = keras.layers.Dense(
-        latent_dims, activation='relu',
-        activity_regularizer=keras.regularizers.l1(lambtha))(encoder_layers[-1])
-    encoder = keras.models.Model(encoder_input, encoder_output, name='encoder')
+    encoder_output = \
+        keras.layers.Dense(
+            latent_dims,
+            activation='relu',
+            activity_regularizer=keras.regularizers.l1(lambtha)
+        )(encoder_layers[-1])
+    encoder = keras.models.Model(encoder_input,
+                                 encoder_output, name='encoder')
 
     decoder_input = keras.layers.Input(shape=(latent_dims,))
     decoder_layers = []
@@ -23,7 +27,8 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
             nodes, activation='relu')(decoder_input))
     decoder_output = keras.layers.Dense(
         input_dims, activation='sigmoid')(decoder_layers[-1])
-    decoder = keras.models.Model(decoder_input, decoder_output, name='decoder')
+    decoder = keras.models.Model(decoder_input,
+                                 decoder_output, name='decoder')
 
     autoencoder_input = keras.layers.Input(shape=(input_dims,))
     encoded = encoder(autoencoder_input)
@@ -31,6 +36,7 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     autoencoder_model = keras.models.Model(
         autoencoder_input, decoded, name='autoencoder')
 
-    autoencoder_model.compile(optimizer='adam', loss='binary_crossentropy')
+    autoencoder_model.compile(optimizer='adam',
+                              loss='binary_crossentropy')
 
     return encoder, decoder, autoencoder_model
