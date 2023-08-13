4b2bb23923af97ff91823e7123938bde2be0ff52
xcoder19 <hedihamida98@gmail.com>
2023-08-13 23:26:55 +0100

    pep8

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -7,11 +7,11 @@ import numpy as np
 def deep_rnn(rnn_cells, X, h_0):
     """deep rnn"""
     t, m, i = X.shape
-    l = len(rnn_cells)
+    l_ = len(rnn_cells)
     h = h_0.shape[2]
     o = rnn_cells[-1].Wy.shape[1]
 
-    H = np.zeros((t + 1, l, m, h))
+    H = np.zeros((t + 1, l_, m, h))
     Y = np.zeros((t, m, o))
 
     H[0] = h_0
@@ -22,7 +22,7 @@ def deep_rnn(rnn_cells, X, h_0):
 
         h_next_layer = [h_prev_layer[0]]
 
-        for layer in range(1, l):
+        for layer in range(1, l_):
             rnn_cell = rnn_cells[layer]
             h_prev = h_next_layer[layer - 1]
             h_next, _ = rnn_cell.forward(h_prev, h_prev_layer[layer])
