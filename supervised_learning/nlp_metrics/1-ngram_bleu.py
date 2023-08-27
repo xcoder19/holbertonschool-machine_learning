5021e6a11e458c26a51b7fe1685f5e1488f79506
hedi <hedi@pop-os.localdomain>
2023-08-27 20:51:47 +0100

    ngram

---
+++ supervised_learning/nlp_metrics/1-ngram_bleu.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""ngram_bleu"""
+
+
+def ngram_bleu(references, sentence, n):
+    """ngram_bleu"""
+
+    return references
---
+++ supervised_learning/nlp_metrics/2-cumulative_bleu.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+"""ngram_precision"""
+def ngram_precision(reference, candidate, n):
+    """ngram_precision"""
+    return reference
